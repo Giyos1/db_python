cabdc74fe5c3c7a1fbda2a802ba74bbc73e380c8
Giyos1 <giyosoripov4@gmail.com>
2025-02-16 10:55:00 +0500

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+class Avto:
+    def save(self):
+        pass
+
+    def update(self, **kwargs):
+        pass
+
+    def show_list(self):
+        avtolar = []
+        # db hamma avtolardi select
+        # qilib iolib kelsaila va Avto classida object olasilar va ularni
+        # avtolar listiga append qilib return
+        return avtolar
+
+    def delete(self):
+        pass
+
+    def filter(self, **kwargs):
+        avtolar = []
+        # db hamma avtolardi select
+        # qilib iolib kelsaila va Avto classida object olasilar va ularni
+        # avtolar listiga append qilib return
+        return avtolar
